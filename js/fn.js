5e94d6a036105c8fc1d33e4e9bb2bfae69f83b59
JeonMinJae <ssi02014@naver.com>
2021-11-09 00:13:03 +0900

    Add: 예제2, 예제3

--- js/fn.js
+++ js/fn.js
@@ -1,5 +1,12 @@
 const fn = {
   add: (num1, num2) => num1 + num2,
+  makeUser: (name, age) => {
+    return {
+      name,
+      age,
+      gender: undefined,
+    };
+  },
 };
 
 module.exports = fn;
--- js/fn.test.js
+++ js/fn.test.js
@@ -12,3 +12,34 @@ test("2더하기 3은 5야", () => {
 test("3더하기 3은 5가 아니야", () => {
   expect(fn.add(3, 3)).not.toBe(5);
 });
+
+test("2더하기 3은 5야", () => {
+  expect(fn.add(2, 3)).toEqual(5);
+});
+
+// 객체나 배열은 재귀적으로 돌면서 값을 확인하기 때문에 toBe가 아닌 toEqual을 사용해야 함
+test("이름과 나이를 전달받아서 객체를 반환해줘", () => {
+  expect(fn.makeUser("minjae", 27)).toEqual({ name: "minjae", age: 27 });
+});
+
+test("이름과 나이를 전달받아서 객체를 반환해줘", () => {
+  expect(fn.makeUser("minjae", 27)).toStrictEqual({
+    name: "minjae",
+    age: 27,
+    gender: undefined,
+  });
+});
+
+const x = 1;
+
+test("null은 null이다.", () => {
+  expect(null).toBeNull();
+});
+
+test("undefined는 undefined이다.", () => {
+  expect(undefined).toBeUndefined();
+});
+
+test("x는 정의되어 있다.", () => {
+  expect(x).toBeDefined();
+});
