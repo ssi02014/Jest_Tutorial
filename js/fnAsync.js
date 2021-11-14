4b98a9e5d39c1016bc3db91a70b02b532a875c04
JeonMinJae <ssi02014@naver.com>
2021-11-14 21:00:06 +0900

    Add(minjae): 비동기 코드 테스트

---
+++ js/fnAsync.js
@@ -0,0 +1,19 @@
+const fnAsync = {
+  add: (num1, num2) => num1 + num2,
+  getName: (callback) => {
+    const name = "Mike";
+    setTimeout(() => {
+      callback(name);
+    }, 3000);
+  },
+  getAge: () => {
+    const age = 27;
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        res(age);
+      }, 3000);
+    });
+  },
+};
+
+module.exports = fnAsync;
